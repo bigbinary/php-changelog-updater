2e3ac751d99bf74b3e24d543899eb9bcd0bdfead
Abhay V Ashokan <abhayvashokan@gmail.com>
2022-09-23 18:15:50 +0530

    Removed unreachable code

--- app/Actions/InsertReleaseNotesInChangelogAction.php
+++ app/Actions/InsertReleaseNotesInChangelogAction.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Actions;
 
-use App\Exceptions\ReleaseNotesCanNotBeplacedException;
 use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
 use League\CommonMark\Node\Block\Document;
 
@@ -14,31 +13,11 @@ class InsertReleaseNotesInChangelogAction
     {
     }
 
-    /**
-     * @throws ReleaseNotesCanNotBeplacedException
-     */
     public function execute(Document $changelog, string $releaseNotes, Heading $newReleaseHeading, ?Heading $unreleasedHeading): Document
     {
         // Prepare raw release notes to be inserted into CHANGELOG.
         $parsedReleaseNotes = $this->prepareReleaseNotes->execute($releaseNotes, $newReleaseHeading);
-
-        // If unreleased heading exists, insert new release notes block **after** this heading.
-        if ($unreleasedHeading !== null) {
-            $unreleasedHeading->insertAfter($parsedReleaseNotes);
-
-            return $changelog;
-        }
-
-        // If no unreleased heading exists in the document, we consider the CHANGELOG empty.
-        // Insert the release notes at the end of the document (after the last element in the existing CHANGELOG).
-        if ($changelog->lastChild() !== null) {
-            $changelog->lastChild()->insertAfter($parsedReleaseNotes);
-
-            return $changelog;
-        }
-
-        // If the CHANGELOG doesn't have any children, we currently don't insert the release notes.
-        // An exception is thrown and an error message is displayed to the user.
-        throw new ReleaseNotesCanNotBeplacedException();
+        $unreleasedHeading->insertAfter($parsedReleaseNotes);
+        return $changelog;
     }
 }
