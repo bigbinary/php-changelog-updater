a5a49f482b95cf32a0b3b1d04234acc6fafb4980
Abhay V Ashokan <abhayvashokan@gmail.com>
2022-09-24 15:05:21 +0530

    Added link to neeto-engineering docs

--- app/Exceptions/UnreleasedHeadingDoesNotExistException.php
+++ app/Exceptions/UnreleasedHeadingDoesNotExistException.php
@@ -10,6 +10,6 @@ class UnreleasedHeadingDoesNotExistException extends Exception
 {
     public function __construct()
     {
-        parent::__construct("The 'Unreleased' heading was not found in the CHANGELOG. Please refer to the correct CHANGELOG format at: https://github.com/bigbinary/neeto-editor/blob/master/CHANGELOG.md.");
+        parent::__construct("The 'Unreleased' heading was not found in the CHANGELOG. Please refer to the correct CHANGELOG format at: https://neeto-engineering.neetokb.com/articles/add-changelog-to-tooling-projects.");
     }
 }
